281584deaae8aefb78c3089734dea4f7c0156856
Jasmin Kenjar <jasminkenjar@2023-JasminKenjar.lan>
2024-11-29 21:09:56 -0600

    A

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const shortid = require("shortid");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+connect_to_db();
+
+const Schema = mongoose.Schema;
+const exercise_info_schema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+});
+const user_schema = new Schema({
+  username: { type: String, required: true },
+});
+const log_schema = new Schema({
+  username: { type: String, required: true },
+  count: { type: Number, required: true },
+  log: {
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+  },
 });
 
+const ExerciseInfo = mongoose.model("exercise_info", exercise_info_schema);
+const User = mongoose.model("user", user_schema);
+const Log = mongoose.model("user", log_schema);
 
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-
+function connect_to_db() {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
